f852de68f9a6b295ecb8e899b98001e443660ccc
dwi purnomo <dwipurnomo.yk@gmail.com>
2017-04-23 21:52:02 +0700

    Add database schema

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from openedoo.core.libs import Blueprint
+from .database import Eclass
+
 
 module_eclass = Blueprint('module_eclass', __name__)
 
+
 @module_eclass.route('/', methods=['POST', 'GET'])
 def index():
-	return "Hello module module_eclass"
+    return "Hello module module_eclass"
---
+++ database.py
@@ -0,0 +1,24 @@
+from openedoo_project import db
+
+
+class Eclass(db.Model):
+    """Eclass database schema
+
+    :id integer: The Eclass id
+    :name varchar: The Eclass name
+    :course varchar: The course where Eclass belongs to
+    :university varchar: The university where Eclass belongs to
+    :member text: The Eclass member
+    :admin text: The Eclass administrator
+    :privilege varchar: The Eclass privilege, public or private
+    :unique_code text: The Eclass unique code
+    """
+    __tablename__ = 'icampus_eclass'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.VARCHAR(50), nullable=False)
+    course = db.Column(db.VARCHAR(50))
+    university = db.Column(db.VARCHAR(50))
+    member = db.Column(db.UnicodeText())
+    admin = db.Column(db.UnicodeText())
+    privilege = db.Column(db.VARCHAR(8), default="private")
+    unique_code = db.Column(db.Text)

75b5334dca84a5904b15a46ebbb5c4e4a92911bb
Dwi purnomo <dwipurnomo.yk@gmail.com>
2017-05-24 19:32:57 +0700

    Make membership relation as separate table

--- database.py
+++ database.py
@@ -9,21 +9,15 @@ class EclassSchema(db.Model):
     :name varchar: The Eclass name
     :course varchar: The course where Eclass belongs to
     :university varchar: The university where Eclass belongs to
-    :member text: The Eclass member
-    :admin text: The Eclass administrator
     :privilege varchar: The Eclass privilege, public or private
-    :unique_code text: The Eclass unique code
     """
 
-    __tablename__ = 'eclass_description'
+    __tablename__ = 'eclass'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(50), nullable=False)
     course = db.Column(db.VARCHAR(50))
     university = db.Column(db.VARCHAR(50))
-    member = db.Column(db.UnicodeText())
-    admin = db.Column(db.UnicodeText())
     privilege = db.Column(db.VARCHAR(8), default="private")
-    unique_code = db.Column(db.Text)
 
 
 class EclassPostsSchema(db.Model):
@@ -46,3 +40,19 @@ class EclassPostsSchema(db.Model):
     attachment = db.Column(db.Text)
     created = db.Column(db.DateTime)
     edited = db.Column(db.DateTime)
+
+
+class EclassMemberSchema(db.Model):
+    """Eclass member database schema.
+
+    :id integer: The membership id
+    :class_id integer: The eclass id
+    :user_id integer: The user id
+    :is_creator integer: (bool) The creator flag
+    """
+
+    __tablename__ = 'eclass_member'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    class_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    is_creator = db.Column(db.SmallInteger, nullable=False, default=0)

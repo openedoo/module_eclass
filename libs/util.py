9cfa8718ed0ada9d714c7edd5b750da5a03a1e48
Dwi Purnomo <dwipurnomo.yk@gmail.com>
2017-05-26 14:59:32 +0700

    Add simpler unique code generator

--- libs/util.py
+++ libs/util.py
@@ -1,4 +1,7 @@
 import tempfile
+import random
+import warnings
+import string
 import os
 
 
@@ -8,6 +11,24 @@ def generate_unique_code(prefix='ecl-'):
     The python uuid module generates too long code. In order to keep it short,
     it uses tempfile.NamedTemporaryFile.
     """
+
+    warnings.warn('Function generate_unique_code is deprecated, use {} instead'
+                  .format('unique_code_generator'), DeprecationWarning,
+                  stacklevel=3)
+
     temporary = tempfile.NamedTemporaryFile(prefix=prefix)
     unique_code = os.path.basename(temporary.name)
+    return unique_code
+
+
+def unique_code_generator(length=8, charset=None):
+    """Generates a short and easy to remember unique_code."""
+    if not charset:
+        ascii_uc = string.ascii_uppercase
+        digit = string.digits
+        charset = ascii_uc + digit
+
+    sys_rand = random.SystemRandom()
+    random_chars = [sys_rand.choice(charset) for _ in range(length)]
+    unique_code = ''.join(random_chars)
     return unique_code

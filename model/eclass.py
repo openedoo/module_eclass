e786561dec45b8877406120cea40a5022c7b211d
Dwi purnomo <dwipurnomo.yk@gmail.com>
2017-06-13 22:03:54 +0700

    Remove unused code

--- model/eclass.py
+++ model/eclass.py
@@ -96,58 +96,6 @@ class Eclass(object):
         except Exception as e:
             return simple_error_message(str(e))
 
-    def get_member(self, eclass_id=None, member_id=None):
-        """Get an eclass member"""
-
-        try:
-            members = self.get_members(eclass_id)
-            member_list = members.split(',')
-
-            result = {'member': False}
-            if member_id in member_list:
-                result = {
-                    'member': True,
-                    'user_id': member_id
-                }
-
-            return result
-
-        except Exception as e:
-            return simple_error_message(str(e))
-
-    def delete_member(self, eclass_id, member_id):
-        """Delete a member in an eclass"""
-
-        try:
-            members = self.get_members(eclass_id)
-            member_list = members.split(',')
-
-            if member_id in member_list:
-                member_list.remove(member_id)
-                members = ','.join(member_list)
-                data = {
-                    'member': members,
-                    'id': eclass_id
-                }
-                update = self.update(data)
-
-            return {'member': False}
-
-        except Exception as e:
-            return simple_error_message(str(e))
-
-    def get_members(self, eclass_id=None):
-        """Get eclass members"""
-
-        try:
-            get_eclass = self.get(eclass_id)
-            parse = json.loads(get_eclass)
-            members = parse[0]['member']
-            return members
-
-        except Exception as e:
-            return simple_error_message(str(e))
-
     def get_admins(self, eclass_id=None):
         """Get eclass admins"""
 

899e10c9c802a3d9408d1badd2f856a679d5afcb
Dwi purnomo <dwipurnomo.yk@gmail.com>
2017-07-21 20:28:11 +0700

    Test create eclass failed with 415 status code

--- tests/test_eclass_api.py
+++ tests/test_eclass_api.py
@@ -12,3 +12,14 @@ class EclassApiTest(unittest.TestCase):
         data = json.loads(eclass.data)
         self.assertEqual(eclass.status_code, 200)
         self.assertEqual(data, "[]")
+
+    def test_create_eclass_failed_with_415_status_code(self):
+        com_science_eclass = {
+            'name': 'computer science',
+            'university': 'Yogyakarta International University',
+            'course': 'IT'
+        }
+        create_eclass = self.client.post('api/v1/eclass',
+                                         data=json.dumps(com_science_eclass))
+        self.assertEqual(create_eclass.status_code, 415)
+        self.assertIn('Failed to load request data', str(create_eclass.data))

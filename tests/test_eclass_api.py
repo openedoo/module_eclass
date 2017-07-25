528dd374c445af8ad907b47b2d4621293204ae35
Dwi purnomo <dwipurnomo.yk@gmail.com>
2017-07-25 22:39:25 +0700

    Test sucesfuly delete member from an eclass

--- tests/test_eclass_api.py
+++ tests/test_eclass_api.py
@@ -268,3 +268,35 @@ class EclassApiTest(unittest.TestCase):
                                   content_type='application/json')
         self.assertEqual(member.status_code, 415)
         self.assertIn('Failed to load request data', str(member.data))
+
+    def test_successfuly_delete_a_member_from_eclass(self):
+        com_science_eclass = {
+            'user_id': 999,
+            'name': 'computer science',
+            'university': 'Yogyakarta International University',
+            'course': 'IT',
+            'privilege': 'public'
+        }
+        create_cs_eclass = self.client.post('api/v1/eclass',
+                                            data=json.dumps(com_science_eclass),
+                                            content_type='application/json')
+        self.assertEqual(create_cs_eclass.status_code, 200)
+        eclass = self.client.get('api/v1/eclass/1')
+        self.assertEqual(eclass.status_code, 200)
+        self.assertIn('computer science', str(eclass.data))
+        student = {
+            'user_id': 67
+        }
+        member = self.client.post('api/v1/eclass/1/members',
+                                  data=json.dumps(student),
+                                  content_type='application/json')
+        self.assertEqual(member.status_code, 200)
+        self.assertIn('success', str(member.data))
+
+        member = self.client.get('api/v1/eclass/1/members')
+        self.assertEqual(member.status_code, 200)
+        self.assertIn('67', str(member.data))
+
+        leave_eclass = self.client.delete('api/v1/eclass/1/members/67')
+        self.assertEqual(leave_eclass.status_code, 200)
+        self.assertIn('success', str(leave_eclass.data))
